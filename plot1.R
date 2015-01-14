8a06abad5782788dd90eaf4d949da70e6f054cbc
Isaac Ben-Akiva <isaac.ben-akiva@ubimobitech.com>
2015-01-14 23:32:56 +0000

    first commit

---
+++ plot1.R
@@ -0,0 +1,12 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+EmissionsTotals <- aggregate(Emissions ~ year, NEI, FUN=sum)
+
+barplot(round(EmissionsTotals[,2]/1000,2), names.arg=EmissionsTotals$year,
+        xlab="Year",
+        ylab="PM2.5 Emissions in Kilo Tons)",
+        main="Total PM2.5 Emissions from All Sources in the United States")
+
+dev.copy(png, file = "./plot1.png", width = 480, height = 480)
+dev.off()
